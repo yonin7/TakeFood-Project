ca9c7cd4945b3646bfddf34e81c13d1469dc7756
yonin7 <92482173+yonin7@users.noreply.github.com>
2022-02-11 02:24:30 +0200

    axios

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AuthState from "./context/auth/authState";
-import CartState from "./context/cart/cartState";
-import ProductState from "./context/products/productState";
-import LoadingState from "./context/loading/loadingState";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AuthState from './context/auth/authState';
+import CartState from './context/cart/cartState';
+import ProductState from './context/products/productState';
+import LoadingState from './context/loading/loadingState';
+import axios from 'axios';
 
-import App from "./App";
+import App from './App';
 
-const rootElement = document.getElementById("root");
-axios.defaults.baseURL = "https://foodeazy-web.herokuapp.com";
+const rootElement = document.getElementById('root');
+axios.defaults.baseURL = 'https://takebis.herokuapp.com';
 ReactDOM.render(
   <React.StrictMode>
     <AuthState>
